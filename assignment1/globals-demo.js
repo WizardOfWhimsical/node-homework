17b54c9c574139be4a18807bf2164cf2b169dfc1
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-03-25 16:41:02 -0400

    feat[globals-demo.js]: added requirements to test

--- assignment1/globals-demo.js
+++ assignment1/globals-demo.js
@@ -1,7 +1,10 @@
 // Log __dirname and __filename
-
+console.log("__dirname:", __dirname);
+console.log("__filename:", __filename);
 
 // Log process ID and platform
-
+console.log();
+console.log();
 
 // Attach a custom property to global and log it
+console.log();
