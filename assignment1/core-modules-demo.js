77f4f762ba4ed59a48d9989b30c0a89049e32a16
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-03-25 17:17:57 -0400

    feat[core-module-demo.js]: OS module info done

--- assignment1/core-modules-demo.js
+++ assignment1/core-modules-demo.js
@@ -1,18 +1,22 @@
-const os = require('os');
-const path = require('path');
-const fs = require('fs');
+const os = require("os");
+const path = require("path");
+const fs = require("fs");
 
-const sampleFilesDir = path.join(__dirname, 'sample-files');
+const sampleFilesDir = path.join(__dirname, "sample-files");
 if (!fs.existsSync(sampleFilesDir)) {
   fs.mkdirSync(sampleFilesDir, { recursive: true });
 }
 
 // OS module
-
+const platform = os.platform();
+const cpu = os.cpus();
+const total_memory = os.totalmem();
+console.log("Platform:", platform);
+console.log("CPU:", cpu);
+console.log("Total Memory:", total_memory);
 
 // Path module
 
 // fs.promises API
-
 
 // Streams for large files- log first 40 chars of each chunk
