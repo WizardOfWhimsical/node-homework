4311d6807a78eadf3a28e4a161652e2c77b130a9
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-03-25 17:09:30 -0400

    fix[globals-demo.js]: test passed

--- assignment1/globals-demo.js
+++ assignment1/globals-demo.js
@@ -3,8 +3,9 @@ console.log("__dirname:", __dirname);
 console.log("__filename:", __filename);
 
 // Log process ID and platform
-console.log();
-console.log();
+console.log("Process ID:", process.pid);
+console.log("Platform:", process.platform);
 
 // Attach a custom property to global and log it
-console.log();
+global.myCustomVar = "Hello, global!";
+console.log("Custom global variable:", global.myCustomVar);
