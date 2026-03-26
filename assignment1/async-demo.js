0380fc86729e39a1cad52fea01035a7c0ab12f62
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-03-25 21:06:13 -0400

    feat[async-demo.js]: passed all tests

--- assignment1/async-demo.js
+++ assignment1/async-demo.js
@@ -1,16 +1,47 @@
-const fs = require('fs');
-const path = require('path');
-
+const fs = require("fs");
+const path = require("path");
 
 // Write a sample file for demonstration
+// const samplePath = path.join(__dirname, "sample-files/sample.txt");
+// console.log(samplePath);
+// fs.writeFile(samplePath, "Hello, async world!", (err) => {
+//   // console.log("write failed");
+//   if (err) throw err;
+// });
 
 // 1. Callback style
+// fs.readFile(samplePath, (err, data) => {
+//   if (err) throw err;
+//   console.log("Callback read:", data);
+// });
+const sampleFilePath = path.join(__dirname, "sample-files/sample.txt");
 
+fs.writeFile(sampleFilePath, "Hello, async world!", (err) => {
+  if (err) {
+    console.error("Error writing file:", err);
+    return;
+  }
+  // console.log(`File written successfully to ${sampleFilePath}`);
 
-  // Callback hell example (test and leave it in comments):
+  fs.readFile(sampleFilePath, "utf-8", (err, data) => {
+    if (err) {
+      console.error("Error reading file:", err);
+      return;
+    }
+    console.log("Callback read:", data);
+  });
+});
+// Callback hell example (test and leave it in comments):
 
+// 2. Promise style
+fs.promises
+  .readFile(sampleFilePath, "utf-8")
+  .then((data) => console.log("Promise read:", data));
 
-  // 2. Promise style
-
-
-      // 3. Async/Await style
+// 3. Async/Await style
+async function readingFile() {
+  const data = await fs.promises.readFile(sampleFilePath, "utf-8");
+  if (!data) return;
+  console.log("Async/Await read:", data);
+}
+readingFile();
