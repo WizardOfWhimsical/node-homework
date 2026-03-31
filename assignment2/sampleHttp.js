4a911d296ef66e3f25b78545840aeed9afc9a458
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-03-31 11:42:23 -0400

    feat[sampleHttp.js]: added html string

--- assignment2/sampleHttp.js
+++ assignment2/sampleHttp.js
@@ -9,6 +9,26 @@ const http = require("http");
 //   );
 // });
 
+const htmlString = `
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Clock</h1>
+<button id="getTimeBtn">Get the Time</button>
+<p id="time"></p>
+<script>
+document.getElementById('getTimeBtn').addEventListener('click', async () => {
+    const res = await fetch('/time');
+    const timeObj = await res.json();
+    console.log(timeObj);
+    const timeP = document.getElementById('time');
+    timeP.textContent = timeObj.time;
+});
+</script>
+</body>
+</html>
+`;
+
 const server = http.createServer({ keepAliveTimeout: 60000 }, (req, res) => {
   if (
     req.method === "POST" &&
