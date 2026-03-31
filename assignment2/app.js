86515a6ba6583f76ed6f57c4f60d3f9296ef4460
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-03-31 19:13:44 -0400

    feat[app.js]: basic express server

---
+++ assignment2/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => console.log(`Listening @ port ${3000}`));
+
+module.exports = { app, server };
