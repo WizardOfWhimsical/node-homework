53cb70c7052994bd682347bbbc2fe69e1921f73f
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-03-31 19:19:19 -0400

    feat[app.js]: added basic error handling for express server

--- assignment2/app.js
+++ assignment2/app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+//basic error handling
+app.use((err, req, res, next) => {
+  console.log("Express Server Error Response\n", err.stack);
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message,
+  });
+});
 
-const port = process.env.PORT || 3000;
 const server = app.listen(port, () => console.log(`Listening @ port ${3000}`));
 
 module.exports = { app, server };
