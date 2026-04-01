285efb3847760e3340895f4df95f25fff0f2bd70
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-03-31 20:05:13 -0400

    fix[app.js]:moved to proper directory

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+//basic error handling
+app.use((err, req, res, next) => {
+  console.log("Express Server Error Response\n", err.stack);
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message,
+  });
+});
+
+const server = app.listen(port, () => console.log(`Listening @ port ${3000}`));
+
+module.exports = { app, server };
