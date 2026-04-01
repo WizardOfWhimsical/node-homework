1942f41167a1afd097b027f4baabe7a8734d7bb2
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-04-01 09:07:30 -0400

    complete

--- assignment2/events-intro.js
+++ assignment2/events-intro.js
@@ -1,37 +1,18 @@
-// import { EventEmitter } from "node:events";
 const EventEmitter = require("events");
-const fs = require("fs");
-
 const emitter = new EventEmitter();
-const process = fs.promises;
-
-// process.writeFile("test.txt", "playing with options");
-// async function reading() {
-//   const something = await process
-//     .readFile(__dirname + "/test.txt", "utf-8")
-//     .then((response) => response);
-//   return something;
-// }
-const something = process
-  .readFile(__dirname + "/test.txt", "utf-8")
-  .then((response) => response);
 
 emitter.on("tell", (message) => {
-  console.log("listener 1 got a tell message:\n", message);
+  console.log("listener 1 got a tell message:", message);
 });
 
-emitter.on("secret", async (message) => {
-  console.log("listener 2 got a tell message:\n", await message);
+emitter.on("tell", (message) => {
+  console.log("listener 2 got a tell message:", message);
 });
+
 emitter.on("error", (error) => {
-  console.log("listener 2 got a tell message:\n", error);
+  console.log("The emitter reported an error.", error.message);
 });
 
-//emitting the event to the listeners
 emitter.emit("tell", "Hi there!");
 emitter.emit("tell", "second message");
-// emitter.emit("secret", reading());
-emitter.emit(
-  "secret",
-  something.then((r) => r),
-);
+emitter.emit("tell", "all done");
--- assignment2/events.js
+++ assignment2/events.js
@@ -2,15 +2,12 @@ const EventEmitter = require("events");
 const emitter = new EventEmitter();
 
 emitter.on("time", (time) => {
-  console.log("Time revieved\n", time);
+  console.log("Time recieved:", time);
 });
 
-function getTime() {
-  const time = new Date().toLocaleTimeString();
-  // return setInterval(() => {
-  return emitter.emit("time", time);
-  // }, 5000);
-}
+setInterval(() => {
+  const currentTime = new Date().toLocaleString();
+  emitter.emit("time", currentTime);
+}, 5000);
 
-// getTime();
-module.exports = { getTime, emitter };
+module.exports = emitter;
