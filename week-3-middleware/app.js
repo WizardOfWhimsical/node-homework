76b0bba01faa01185244705701c5f2efe1c9c69b
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-04-03 12:01:58 -0400

    feat[app.js]: added .json & .urlencoded for form data

--- week-3-middleware/app.js
+++ week-3-middleware/app.js
@@ -5,7 +5,18 @@ const dogsRouter = require("./routes/dogs");
 
 const app = express();
 
+app.use(express.json()); // Parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Parse form data
+
 // Your middleware here
+
+app.use((req, res, next) => {
+  console.log(
+    "Coming/Going:\n",
+    `\t${new Date().toLocaleString()}: ${req.method} ${req.path}`,
+  );
+  next();
+});
 
 app.use("/", dogsRouter); // Do not remove this line
 
@@ -14,3 +25,16 @@ const server = app.listen(3000, () =>
 );
 
 module.exports = server;
+
+// 2. Body parsing (before routes that need req.body)
+
+// 5. 404 handler (after all routes)
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+// 6. Error handler (last - catches all errors)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal server error" });
+});
