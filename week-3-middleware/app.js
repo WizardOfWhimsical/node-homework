ac1f09c090e1a6aab959e42047afab11ef26848b
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-04-03 12:07:07 -0400

    feat[app.js]: added route to register user

--- week-3-middleware/app.js
+++ week-3-middleware/app.js
@@ -19,6 +19,10 @@ app.use((req, res, next) => {
 });
 
 app.use("/", dogsRouter); // Do not remove this line
+app.post("/api/user/register", (req, res) => {
+  const payload = req.body.json();
+  res.status().json({ message: "Data Recieved", payload });
+});
 
 const server = app.listen(3000, () =>
   console.log("Server listening on port 3000"),
