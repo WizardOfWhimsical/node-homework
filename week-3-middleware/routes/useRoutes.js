a3d87e22522609fe67482f00009b5c4c67df24ca
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-04-05 23:01:13 -0400

    feat[useRouter.js]: finished post route for register

--- week-3-middleware/routes/useRoutes.js
+++ week-3-middleware/routes/useRoutes.js
@@ -1,3 +1,8 @@
 const { Router } = require("express");
+const register = require("../controllers/userControler");
 
 const router = Router();
+
+router.route("/register").post(register);
+
+module.exports = router;
