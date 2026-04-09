437ff21f6ec137d1b4131a84bac2bc0c964e5d70
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-04-08 22:31:26 -0400

    all test passed

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,50 +10,40 @@ function register(req, res) {
   global.users.push(newUser);
   global.user_id = newUser;
   console.log("Register New User\n", newUser);
-  delete newUser.password;
+  delete req.body.password;
   res
     .status(StatusCodes.CREATED)
-    .json({ message: newUser, reason: ReasonPhrase.CREATED });
+    .json({ ...req.body, message: "Account Created" });
 }
 
 function logon(req, res) {
-  console.log("Logon Request Body:\n", req.body);
-
   const { email, password } = req.body;
   const user = global.users.find((user) => user.email === email);
+
   if (!user) {
     console.log("user not found");
     return res.status(StatusCodes.NOT_FOUND).json({
-      reason: ReasonPhrase.NOT_FOUND,
       message: "Please Register an Account",
     });
   }
 
   if (password !== user.password) {
-    console.log("wrong password");
     return res.status(StatusCodes.UNAUTHORIZED).json({
-      reason: ReasonPhrase.UNAUTHORIZED,
       message: "Authentication Failed",
     });
   } else if (password === user.password) {
     console.log("login successful");
     user.isLoggedIn = true;
     global.user_id = user;
-    console.log("official user\n", user);
-    delete user.password;
-    res.status(StatusCodes.ACCEPTED).json({
-      reason: ReasonPhrase.ACCEPTED,
-      message: "Successfully logged in",
-      user,
-    });
+    res
+      .status(StatusCodes.OK)
+      .json({ name: user.name, email: user.email, message: "logged in" });
   }
 }
 
 function logoff(req, res) {
   global.user_id = null;
-  res
-    .status(StatusCodes.OK)
-    .json({ reason: ReasonPhrase.OK, message: "Logout Successful" });
+  res.status(StatusCodes.OK).json({ message: "logged out" });
 }
 
 module.exports = { register, logon, logoff };
