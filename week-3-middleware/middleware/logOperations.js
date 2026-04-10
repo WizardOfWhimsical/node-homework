7129498d01cb0c21983acb42ffb938d9114fa953
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-04-10 14:51:05 -0400

    fix: forgot next()

--- week-3-middleware/middleware/logOperations.js
+++ week-3-middleware/middleware/logOperations.js
@@ -2,5 +2,6 @@ function loggingOperations(req, res, next) {
   req.timeStamp = new Date().toLocaleTimeString();
   const { timeStamp, method, path, requestId } = req;
   console.log(`[${timeStamp}]: ${method} ${path}(${requestId})`);
+  next();
 }
 module.exports = loggingOperations;
