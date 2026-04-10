f5256689762c439141c969f139b2a279ee688520
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-04-10 11:50:40 -0400

    fix[dog.js]: removed unnessary deconstruction

--- week-3-middleware/routes/dogs.js
+++ week-3-middleware/routes/dogs.js
@@ -3,22 +3,22 @@ const router = express.Router();
 const dogs = require("../dogData.js");
 
 router.get("/dogs", (req, res) => {
-	res.json(dogs);
+  res.json(dogs);
 });
 
 router.post("/adopt", (req, res) => {
-    const { name, address, email, dogName } = req.body;
-    if (!name || !email || !dogName) {
-        return res.status(400).json({ error: "All fields are required" });
-    }
+  const { name, email, dogName } = req.body;
+  if (!name || !email || !dogName) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
 
-    return res.status(201).json({
-        message: `Adoption request received. We will contact you at ${email} for further details.`,
-    });
+  return res.status(201).json({
+    message: `Adoption request received. We will contact you at ${email} for further details.`,
+  });
 });
-
+//ask Ej about this route, it is for testing error handling in the app, it will throw an error when accessed.
 router.get("/error", (req, res) => {
-	throw new Error("Test error");
+  throw new Error("Test error");
 });
 
 module.exports = router;
