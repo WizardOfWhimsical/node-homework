0b50a3d60d8b56bdc07a3723c2f25693e9fe105e
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-04-13 10:27:00 -0400

    feat[dogs.js]: added custom error classes

--- week-3-middleware/routes/dogs.js
+++ week-3-middleware/routes/dogs.js
@@ -8,17 +8,19 @@ router.get("/dogs", (req, res) => {
 });
 
 router.post("/adopt", (req, res) => {
-  const { name, email, dogName } = req.body;
+  const { name, email, dogName, dog } = req.body;
   //validation
   if (!name || !email || !dogName) {
     throw new ValidationError("Missing required fields");
   }
-
   //not found
-  if (!dog || dog.status !== "available")
-    return res.status(201).json({
-      message: `Adoption request received. We will contact you at ${email} for further details.`,
-    });
+  if (!dog || dog.status !== "available") {
+    throw new NotFoundError("Dog not found or not available");
+  }
+  //pessimistic success
+  return res.status(201).json({
+    message: `Adoption request received. We will contact you at ${email} for further details.`,
+  });
 });
 //ask Ej about this route, it is for testing error handling in the app, it will throw an error when accessed?
 router.get("/error", (req, res) => {
