685ac3543d1757df8bb4250d91f089bdae9bba4a
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-04-14 21:51:56 -0400

    fix[app.js]: put middleware together.

--- week-3-middleware/app.js
+++ week-3-middleware/app.js
@@ -9,8 +9,7 @@ const dogsRouter = require("./routes/dogs");
 
 const app = express();
 
-app.use(uniqueId, loggingOperations);
-app.use(securityHeaders);
+app.use(uniqueId, loggingOperations, securityHeaders);
 app.use(express.json({ limit: "1mb" }));
 app.use(express.static("./public"));
 
