cde33fedbaf0182f1647c5fa2d5b3f5295ee607b
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-04-14 21:33:41 -0400

    fix[app.js]FINALLY got rid of the red squigglies

--- week-3-middleware/app.js
+++ week-3-middleware/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const uniqueId = require("./middleware/uniqueId");
 const loggingOperations = require("./middleware/logOperations");
 const securityHeaders = require("./middleware/additionalHeaders");
+const extendedErrorHandling = require("./middleware/customErrorHandling");
 const { NotFoundError } = require("./error");
 const { StatusCodes } = require("http-status-codes");
 const dogsRouter = require("./routes/dogs");
@@ -36,17 +37,6 @@ const server = app.listen(3000, () =>
 app.use((req, res, next) => {
   next(new NotFoundError("Route not found"));
 });
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  if (statusCode >= 400 && statusCode < 500) {
-    console.warn(`WARN: ${err.name} ${err.message}`);
-  } else {
-    console.error(`ERROR: Error ${err.message}`);
-  }
-  res.status(statusCode).json({
-    error: err.message || "Internal Server Error",
-    requestId: req.requestId,
-  });
-});
+app.use(extendedErrorHandling);
 
 module.exports = server;
