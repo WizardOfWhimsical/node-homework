36eac31bb6d1cf9ecfe2caa1b043a40a35e3e580
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-04-14 21:27:04 -0400

    fix[additionalHeaders.js]: cleaned up commented code and googled way to set multiple headers

--- week-3-middleware/middleware/additionalHeaders.js
+++ week-3-middleware/middleware/additionalHeaders.js
@@ -1,12 +1,9 @@
 function securityHeaders(req, res, next) {
-  res.setHeader("X-Content-Type-Options", "nosniff");
-  res.setHeader("X-Frame-Options", "DENY");
-  res.setHeader("X-XSS-Protection", "1; mode=block");
-  // res.setHeader({
-  //   "X-Content-Type-Options": "nosniff",
-  //   "X-Frame-Options": "Deny",
-  //   "X-XSS-Protection": "1; mode=block",
-  // });
+  res.set({
+    "X-Content-Type-Options": "nosniff",
+    "X-Frame-Options": "DENY",
+    "X-XSS-Protection": "1; mode=block",
+  });
   next();
 }
 
