30dfabe2debc924f156bf7180e4c98511afc8f4f
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-04-14 21:23:05 -0400

    fix[dog.js]: cleaned up commented code

--- week-3-middleware/routes/dogs.js
+++ week-3-middleware/routes/dogs.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const router = express.Router();
+const { Router } = require("express");
 const { ValidationError, NotFoundError } = require("../error.js");
-const dogs = require("../dogData.js");
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
+const dogs = require("../dogData.js");
+const router = Router();
 
 router.get("/dogs", (req, res) => {
   res.status(200).json(dogs);
@@ -10,30 +10,28 @@ router.get("/dogs", (req, res) => {
 
 router.post("/adopt", (req, res) => {
   const { name, email, dogName } = req.body;
-  //validation
+
   if (!name || !email || !dogName) {
     throw new ValidationError("Missing required fields", req.requestId);
   }
-  //not found
+
   const dog = dogs.filter((dog) => dog.name === dogName);
-  //returns an array of obj/objs that match
+
   if (!dog[0] || dog[0].status !== "available") {
     throw new NotFoundError("Dog not found or not available", req.requestId);
   }
-  //pessimistic success
+
   return res.status(201).json({
     message: `Adoption request received. We will contact you at ${email} for further details.`,
-    // id:req.requestId
   });
 });
-//ask Ej about this route, it is for testing error handling in the app, it will throw an error when accessed?
+
 router.get("/error", (req, res) => {
   res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
     message: "Internal Server Error",
     requestId: req.requestId,
     error: ReasonPhrases.INTERNAL_SERVER_ERROR,
   });
-  // throw new NotFoundError("Route not found");
 });
 
 module.exports = router;
