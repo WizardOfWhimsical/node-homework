b09f21c30f1ee8e19a1d3b63e01b56a87bb3f02b
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-04-16 00:22:54 -0400

    feat[taskController.js]: created delete function

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,4 +1,4 @@
-const { StatusCodes } = require("../index");
+const { StatusCodes, ReasonPhrases } = require("../index");
 
 function create(req, res) {
   const newTask = {
@@ -13,7 +13,29 @@ function create(req, res) {
 function getTaskList(req, res) {}
 function showTask(req, res) {}
 function editTask(req, res) {}
-function deleteTask(req, res) {}
+
+function deleteTask(req, res) {
+  const taskToFind = parseInt(req.params?.id);
+  if (!taskToFind || typeof taskToFind === "number") {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      message: "The asked for ID is not valid",
+      error: "Invalid Request",
+    });
+  }
+  const taskIndex = global.tasks.findIndex(
+    (task) => task.id === taskToFind && task.userId === global.user_id.email,
+  );
+  if (taskIndex === -1) {
+    return res.status(StatusCodes.NOT_FOUND).json({
+      message: "That task was not found",
+      error: ReasonPhrases.NOT_FOUND,
+    });
+  }
+  //ask about this in the morning. i think this is supposed to be destructured
+  const task = { userId, ...global.tasks[taskIndex] };
+  global.tasks.splice(taskIndex, 1);
+  res.status(StatusCodes.OK).json(task);
+}
 
 const taskCounter = (() => {
   let lastTaskNumber = 0;
