055be875bbb60c9b78ba3463d4e09eadaf5e204a
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-04-18 01:19:40 -0400

    feat[app.js]: installed debuggerLogger middlware for testing

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,19 +1,22 @@
 const chalk = require("chalk");
-const log = console.log();
-const yellow = chalk.yellow();
-const blue = chalk.blue.bold();
-const magenta = chalk.magenta.italic();
+// const console.log = console.console.log;
+// const yellow = chalk.yellow;
+// const blue = chalk.blue;
+// const magenta = chalk.magenta;
 
-function debggerLogger(req, res, next) {
-  log("\n" + blue("=== Incoming Request ==="));
-  log(yellow("Method:"), req.method);
-  log(yellow("Path:"), req.path);
-  log(yellow("Query:"), req.method);
-  log(yellow("Params:"), req.params);
-  log(yellow("Body:"), req.body);
-  log("\n" + blue("Checking Globals"));
-  log(magenta("global.users"), global.users);
-  log(magenta("global.tasks"), global.tasks);
-  log(magenta("global.user_id"), global.user_id);
-  log(blue("=== End ===" + "\n"));
+function debuggerLogger(req, res, next) {
+  console.log("\n" + chalk.blue("=== Incoming Request ==="));
+  console.log(chalk.yellow("Method:"), req.method);
+  console.log(chalk.yellow("Path:"), req.path);
+  console.log(chalk.yellow("Query:"), req.method);
+  console.log(chalk.yellow("Params:"), req.params);
+  console.log(chalk.yellow("Body:"), req.body);
+  console.log("\n" + chalk.blue("Checking Globals"));
+  console.log(chalk.magenta("global.users"), global.users);
+  console.log(chalk.magenta("global.tasks"), global.tasks);
+  console.log(chalk.magenta("global.user_id"), global.user_id);
+  console.log(chalk.blue("=== End ===" + "\n"));
+  next();
 }
+debuggerLogger();
+module.exports = { debuggerLogger };
