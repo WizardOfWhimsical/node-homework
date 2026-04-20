9fc82d3f956dff08bf9a298d3d938d2c4af11c53
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-04-20 12:36:40 -0400

    feat[logger.js]: made logger for globals to be checked on resp

--- middleware/logger.js
+++ middleware/logger.js
@@ -4,19 +4,26 @@ const chalk = require("chalk");
 // const blue = chalk.blue;
 // const magenta = chalk.magenta;
 
-function debuggerLogger(req, res, next) {
+function requestLogger(req, res, next) {
   console.log("\n" + chalk.blue("=== Incoming Request ==="));
-  console.log(chalk.yellow("Method:"), req.method);
-  console.log(chalk.yellow("Path:"), req.path);
-  console.log(chalk.yellow("Query:"), req.method);
-  console.log(chalk.yellow("Params:"), req.params);
-  console.log(chalk.yellow("Body:"), req.body);
-  console.log("\n" + chalk.blue("Checking Globals"));
-  console.log(chalk.magenta("global.users"), global.users);
-  console.log(chalk.magenta("global.tasks"), global.tasks);
-  console.log(chalk.magenta("global.user_id"), global.user_id);
-  console.log(chalk.blue("=== End ===" + "\n"));
+  console.log(chalk.yellow("Method:"), req?.method);
+  console.log(chalk.yellow("Path:"), req?.path);
+  console.log(chalk.yellow("Query:"), req?.query);
+  console.log(chalk.yellow("Params:"), req?.params);
+  console.log(chalk.yellow("Body:"), req?.body);
+
   next();
 }
-debuggerLogger();
-module.exports = { debuggerLogger };
+
+function responseLogger(req, res, next) {
+  res.on("finish", () => {
+    console.log("\n" + chalk.blue("Checking Globals"));
+    console.log(chalk.magenta("global.users"), global?.users);
+    console.log(chalk.magenta("global.tasks"), global?.tasks);
+    console.log(chalk.magenta("global.user_id"), global?.user_id);
+    console.log(chalk.blue("=== End ===" + "\n"));
+  });
+  next();
+}
+
+module.exports = { requestLogger, responseLogger };
