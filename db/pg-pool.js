428f181fff2bc33adfce9ad30b099469535f9957
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-05-05 17:40:04 -0400

    chores: minor changes to things

--- db/pg-pool.js
+++ db/pg-pool.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-
+console.log("database url", process.env.DATABASE_URL);
 pool.on("error", (err, client) => {
   console.error("Unexpected error on idle client", client + "\n", "\t" + err);
 });
--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,12 +1,10 @@
-const { StatusCodes } = require("http-status-codes");
+const { StatusCodes } = require("../index");
 
 function notFound(err, req, res, next) {
   console.log("Erroring, Not found page:\n", req.url);
-  return res
-    .status(StatusCodes.NOT_FOUND)
-    .json({
-      error: `You can't do a ${req.method} for ${req.url}`,
-      requestId: `${req.requestId}`,
-    });
+  return res.status(StatusCodes.NOT_FOUND).json({
+    error: `You can't do a ${req.method} for ${req.url}`,
+    requestId: `${req.requestId}`,
+  });
 }
 module.exports = notFound;
