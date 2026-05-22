ded5f84f1d87c1b7a724ac77326a773d959ca03f
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-05-21 23:59:11 -0400

    feat[cookie]: ccookie flag and jwt generator created

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,15 @@
-const { StatusCodes } = require("../index");
+const { StatusCodes, prisma, jwt, crypto, util } = require("../index");
 const { userSchema } = require("../validation/userSchema");
-const prisma = require("../db/prisma");
-const getPrismaErrorInfo = require("../middleware/customPrismaErrorHandling/getPrismaErrorInfo");
+const getPrismaErrorInfo = require("../middleware/index");
 
-const crypto = require("crypto");
-const util = require("util");
 const scrypt = util.promisify(crypto.scrypt);
+const { randomUUID } = crypto;
+
+// const prisma = require("../db/prisma");
+// const jwt = require("jsonewebtoken")
+// const crypto = require("crypto");
+// const { randomUUID } = crypto;
+// const util = require("util");
 
 /**
  * @param {String} password
@@ -27,6 +31,39 @@ async function comparePassword(inputPassword, storedHash) {
   const keyBuffer = Buffer.from(key, "hex");
   const derivedKey = await scrypt(inputPassword, salt, 64);
   return crypto.timingSafeEqual(keyBuffer, derivedKey);
+}
+
+/**
+ *
+ * @param {Object} req - The Express request object.
+ * @returns
+ * Object {{
+ *    httpOnly:boolean,
+ *    secure: boolean,
+ *    sameSite: string
+ *     }}
+ */
+function cookieFlags(req) {
+  return {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "Strict",
+  };
+}
+
+/**
+ *
+ * @param {Object} req - The Express request object.
+ * @param {Object} res - The Express request object
+ * @param {object} user {{unknown}}
+ * @returns string
+ */
+function setJwtCookie(req, res, user) {
+  const payload = { id: user.id, csrfToken: randomUUID() };
+  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+  res.cookie("jwt", token, { ...cookieFlags(req), maxAge: 3600000 });
+  return payload.csrfToken;
 }
 
 /**
@@ -210,10 +247,8 @@ async function show(req, res, next) {
 }
 
 async function logoff(req, res) {
-  // console.log("hitting");
-  // return;
   if (global.user_id === null) {
-    res
+    return res
       .status(StatusCodes.BAD_REQUEST)
       .json({ message: "Register to login", error: "Noone logged in" });
   }
