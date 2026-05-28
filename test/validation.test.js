c087d3b7db8478f3ad0f78521e5d1a73805a22db
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-05-27 22:36:53 -0400

    feat[jestTest]: 1-7 wrote. 6-7 failing

--- test/validation.test.js
+++ test/validation.test.js
@@ -1,38 +1,77 @@
 const { userSchema } = require("../validation/userSchema");
 const { taskSchema, patchSchema } = require("../validation/taskSchema");
 
-const badPassword = {
+const abortFlag = { abortEarly: false };
+let user = {
   name: "Bob",
   email: "bob@sample.com",
-  password: "password",
+  password: "StrongPassword123",
 };
-const abortFlag = { abortEarly: false };
+let error = null,
+  value = null;
 
-describe("User object validation test", () => {
+describe("User schema validation test", () => {
   it("1. Does not permit a trivial password", () => {
-    const { error } = userSchema(badPassword, abortFlag);
-    /**
-     * The ERROR returned should have and an ARRAY of (detail)
-     * objects, each having a (context) with a (key) for password.
-     * So we use the find() with context.key to see if the password
-     * key exists.
-     */
+    ({ error, value } = userSchema.validate({ ...user }, abortFlag));
+
     expect(
-      error.details.find((detail) => detail.context.key == "password"),
+      error.details.find((detail) => detail.context.key === "password"),
     ).toBeDefined();
   });
-  it("2. The user schema requires email", () => {});
-  it("3. The user schema does not accept invalid email", () => {});
-  it("4. The user Schema requires password", () => {});
-  it("5. The user Schema requires name", () => {});
-  it("6. The user name must be valid (3 - 30 characters", () => {});
-  it("7. If validation is performed on a valid user object, error comes back falsy", () => {});
-  it("8. The task schema requires a title", () => {});
-  it("9. If an isCompleted value is specified, it must be valid", () => {});
-  it("10. If an isCompleted value is not specified but the rest of the object is valid, a default of false is provided by validation", () => {});
-  it("11. If isCompleted in the provided object has the value true, it remains true after validation", () => {});
+
+  it("2. The user schema requires email", () => {
+    ({ email, ...user } = user);
+    ({ error } = userSchema.validate(user, abortFlag));
+
+    expect(
+      error.details.find((detail) => detail.context.key === "email"),
+    ).toBeDefined();
+  });
+  it("3. The user schema does not accept invalid email", () => {
+    ({ error } = userSchema.validate(
+      { ...user, email: "bademail.com" },
+      abortFlag,
+    ));
+
+    expect(
+      error.details.find((detail) => detail.context.key === "email"),
+    ).toBeDefined();
+  });
+
+  it("4. The user Schema requires password", () => {
+    ({ password, ...user } = user);
+    ({ error } = userSchema.validate(user, abortFlag));
+
+    expect(
+      error.details.find((detail) => detail.context.key === "password"),
+    ).toBeDefined();
+  });
+  it("5. The user Schema requires name", () => {
+    ({ name, ...user } = user);
+    ({ error } = userSchema.validate(user, abortFlag));
+    expect(
+      error.details.find((detail) => detail.context.key === "name"),
+    ).toBeDefined();
+  });
+  it("6. The user name must be valid (3 - 30 characters)", () => {
+    ({ error } = userSchema.validate({ ...user, name: "Po" }));
+
+    expect(
+      error.details.find((detail) => detail.context.key === "name"),
+    ).toBeDefined();
+  });
+  it("7. If validation is performed on a valid user object, error comes back falsy", () => {
+    ({ error, value } = userSchema.validate(user, abortFlag));
+    expect(error).toBeNull();
+  });
 });
-describe("Patch Task schema testing", () => {
-  it("12. The patch schema does not require a title", () => {});
-  it("13. If no value i sprovided for isCompleted this remains undefined in the returned value", () => {});
-});
+// describe("Task schema validation testing", () => {
+//   it("8. The task schema requires a title", () => {});
+//   it("9. If an isCompleted value is specified, it must be valid", () => {});
+//   it("10. If an isCompleted value is not specified but the rest of the object is valid, a default of false is provided by validation", () => {});
+//   it("11. If isCompleted in the provided object has the value true, it remains true after validation", () => {});
+// });
+// describe("Patch Task schema testing", () => {
+//   it("12. The patch schema does not require a title", () => {});
+//   it("13. If no value i sprovided for isCompleted this remains undefined in the returned value", () => {});
+// });
