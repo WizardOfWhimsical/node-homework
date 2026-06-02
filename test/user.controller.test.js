3d8843d158255f9a4f660864b1cf2126fe01038f
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-06-01 22:38:24 -0400

    feat[jestTest/user]: security check on headers

--- test/user.controller.test.js
+++ test/user.controller.test.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
 process.env.DATABASE_URL = process.env.TEST_DATABASE_URL;
-const waitForRouteHandlerCompletion = require("./waitForRouteHandlerCompletion");
+const {
+  waitForRouteHandlerCompletions,
+} = require("./waitForRouteHandlerCompletion");
 
 const { prisma, httpMocks, jwt, cookie, EventEmitter } = require("../index");
 
 const { register, logoff, logon } = require("../controllers/index");
 
-const jwtMiddleware = require("../middleware/index");
+const {
+  handleAuthMiddleware: jwtMiddleware,
+  logger,
+} = require("../middleware/index");
 
 let saveRes,
   saveData = null;
@@ -48,14 +53,34 @@ describe("Testing register, logon and log off", () => {
   it("33. User can be registered", async () => {
     const req = httpMocks.createRequest({ method: "POST", body: Bob });
     saveRes = MockResponseWithCookies();
-    await waitForRouteHandlerCompletion(register, req, saveRes);
+    await waitForRouteHandlerCompletions(register, req, saveRes);
     expect(saveRes.statusCode).toBe(201);
   });
 
   it("34. The user can logon", async () => {
     const req = httpMocks.createRequest({ method: "POST", body: Bob });
     saveRes = MockResponseWithCookies();
-    await waitForRouteHandlerCompletion(logon, req, saveRes);
+    await waitForRouteHandlerCompletions(logon, req, saveRes);
     expect(saveRes.statusCode).toBe(200);
   });
-});
+
+  it("35. A string in the cookie array starts with 'jwt='.", async () => {
+    const setCookieArray = saveRes.get("Set-Cookie");
+    jwtCookie = setCookieArray.find((str) => str.startsWith("jwt="));
+    expect(jwtCookie).toBeDefined();
+  });
+
+  it("36. The string contains 'HTTPOnly'.", async () => {
+    const setCookieArray = saveRes.get("Set-Cookie");
+    // logger(setCookieArray[0].split());
+    expect(setCookieArray[0]).toMatch(/HttpOnly/);
+  });
+
+  it("37. The returned data contains a csrfToken.", async () => {
+    const resp = saveRes._getJSONData();
+    logger({ resp });
+    expect(resp).toHaveProperty("csrfToken");
+  });
+
+  it("38. ");
+}); //end of describe
