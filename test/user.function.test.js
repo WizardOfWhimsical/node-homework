e03ff8466e6c895b3cddfc708217493504e58f9b
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-06-02 22:17:01 -0400

    feat[jestTest/server]: register user returns proper obj

--- test/user.function.test.js
+++ test/user.function.test.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 const request = require("supertest");
 process.env.DATA_BASE_URL = process.env.TEST_DATABASE_URL;
 const { prisma } = require("../index");
-let agent, saveRes;
+const { logger } = require("../middleware/index");
 const { app, server } = require("../app");
+let agent, saveRes;
 
 beforeAll(async () => {
   await prisma.Task.deleteMany();
@@ -23,7 +24,26 @@ describe("register a user", () => {
       email: "jdeere@example.com",
       password: "Pa$$word20",
     };
-    saveRes = await agent.post("/user/register").send(newUser);
-    expect(saveRes.status).toBe(200);
+    saveRes = await agent.post("/api/users/register").send(newUser);
+    expect(saveRes.status).toBe(201);
+  });
+
+  it("47. Registration returns an object with the expected name", () => {
+    expect(saveRes.body.user.name).toMatch(/John/);
+  });
+
+  it("48. Test that the returned object includes a csrfToken", () => {
+    logger(saveRes.body);
+    expect(saveRes.body).toHaveProperty("csrfToken");
+  });
+
+  it("49. You can logon as the newly registered user", async () => {
+    const user = {
+      // name: "John Deere",
+      email: "jdeere@example.com",
+      password: "Pa$$word20",
+    };
+    saveRes = await agent.post("/api/users/logon").send(user);
+    expect(saveRes.body.messsage).toMatch(/logged/);
   });
 });
