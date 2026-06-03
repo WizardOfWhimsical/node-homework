610f40e1cebc1fa40c11d80c113d9215e4087e16
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-06-02 20:55:30 -0400

    feat[jestTest/server]: db clear and disconnected, server closed

--- test/user.function.test.js
+++ test/user.function.test.js
@@ -4,3 +4,14 @@ process.env.DATA_BASE_URL = process.env.TEST_DATABASE_URL;
 const { prisma } = require("../index");
 let agent, saveRes;
 const { app, server } = require("../app");
+
+beforeAll(async () => {
+  await prisma.Task.deleteMany();
+  await prisma.User.deleteMsny();
+  agent = request.agent(app);
+});
+
+afterAll(async () => {
+  prisma.$disconnect();
+  server.close();
+});
