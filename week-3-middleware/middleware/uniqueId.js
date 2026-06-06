2b755fb025c0c00b5809febb7381f09e72fc4c4b
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-06-05 20:11:10 -0400

    fix[middleware]:changed uuid for crypto

--- week-3-middleware/middleware/uniqueId.js
+++ week-3-middleware/middleware/uniqueId.js
@@ -1,7 +1,7 @@
 const { randomUUID } = require("crypto");
 
 function setUniqueId(req, res, next) {
-  req.requestId = randomUUID;
+  req.requestId = randomUUID();
   res.setHeader("X-Request-Id", req.requestId);
   next();
 }
