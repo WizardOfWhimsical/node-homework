30ad699e7c82109f847a4142c8ae682f3e04c612
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-06-11 16:05:00 -0400

    feat: hitting google getting tokens back, to send out for user info and recieved.

--- controllers/googleLogon.js
+++ controllers/googleLogon.js
@@ -1,5 +1,7 @@
 const { OAuth2Client } = require("google-auth-library"); //
+const { logger } = require("../middleware/index");
 require("dotenv").config();
+
 // 1. Build the client with your exact keys
 const oAuth2Client = new OAuth2Client({
   clientId: process.env.GOOGLE_CLIENT_ID,
@@ -19,9 +21,23 @@ async function googleLogon(req, res) {
       code: code,
       redirectUri: "postmessage",
     }); //
-
+    logger({ r });
     // 4. Save the tokens to the client
     oAuth2Client.setCredentials(r.tokens); //
+    // 5. get user info
+    const idToken = r.tokens.id_token;
+    const userInfo = await oAuth2Client.verifyIdToken({
+      idToken,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+    const payload = userInfo.getPayload();
+    logger(payload);
+    // 6. use info to query db
+    // 7. if Exist ? logon : register;
+    // 8. continue through register w/o pw
+    // 9a. send response to front end like business as usual
+    // 9b. their tasks explain a set password
+    logger(oAuth2Client);
 
     // Send a success message back to the frontend
     res.status(200).json({ message: "Tokens acquired!" });
