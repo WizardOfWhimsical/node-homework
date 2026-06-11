2447209fa17e4e30711c2385dbb2d02639d3ade6
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-06-11 16:22:50 -0400

    feat:moved password protection to newly created security dir

---
+++ security/passwordProtection.js
@@ -0,0 +1,30 @@
+const { crypto, util } = require("../index");
+// const { userSchema } = require("../validation/userSchema");
+// const { getPrismaErrorInfo } = require("../middleware/index");
+
+const scrypt = util.promisify(crypto.scrypt);
+// const { randomUUID } = crypto;
+
+/**
+ * @param {String} password
+ * @returns string salted/hashed
+ */
+async function hashPassword(password) {
+  const salt = crypto.randomBytes(16).toString("hex");
+  const derivedKey = await scrypt(password, salt, 64);
+  return `${salt}:${derivedKey.toString("hex")}`;
+}
+
+/**
+ * @param {String}
+ * @param {String}
+ * @returns boolean of comparison
+ */
+async function comparePassword(inputPassword, storedHash) {
+  const [salt, key] = storedHash.split(":");
+  const keyBuffer = Buffer.from(key, "hex");
+  const derivedKey = await scrypt(inputPassword, salt, 64);
+  return crypto.timingSafeEqual(keyBuffer, derivedKey);
+}
+
+module.exports = { hashPassword, comparePassword };
