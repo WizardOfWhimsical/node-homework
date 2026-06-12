be1f9df786ef8d6bfad8c9ee8ea5d1d7d9b8f213
WizardOfWhimsical <st.rayis1085@gmail.com>
2026-06-11 22:03:57 -0400

    feat:encapsulated regestry

--- security/googleLogon.js
+++ security/googleLogon.js
@@ -11,22 +11,92 @@ const oAuth2Client = new OAuth2Client({
   redirectUri: "postmessage",
 });
 
-async function getExistingUser(email) {
-  return await prisma.user.findUnique({
-    where: { email },
-    select: {
-      id: true,
-      name: true,
-      email: true,
-      createdAt: true,
-      hashedPassword: true,
-    },
-  });
+async function googleLogon(req, res, next) {
+  console.log("google-hand-shake");
+  try {
+    const code = req.body.code;
+
+    const r = await oAuth2Client.getToken({
+      code: code,
+      redirectUri: "postmessage",
+    });
+
+    oAuth2Client.setCredentials(r.tokens);
+    // 5. get user info
+    const idToken = r.tokens.id_token;
+
+    const userInfo = await oAuth2Client.verifyIdToken({
+      idToken,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+
+    const payload = userInfo.getPayload();
+    const splitEmail = (email) => {
+      return email?.split("@")[0];
+    };
+
+    const { name, email } = payload;
+
+    const candidateUser = {
+      name,
+      email,
+      password: `${splitEmail(payload?.email)}${process.env.GOOGLE_CLIENT_PASSWORD_SALT}`,
+    };
+
+    // 6. use info to query db
+    let existingUser;
+    try {
+      existingUser = await prisma.user.findUnique({
+        where: { email },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          createdAt: true,
+          hashedPassword: true,
+        },
+      });
+    } catch (err) {
+      getPrismaErrorInfo(err);
+      return next(err);
+    }
+
+    if (!existingUser) {
+      //start register
+      return registerUser(req, res, candidateUser, next);
+    } //end of register
+
+    const compairison = await comparePassword(
+      candidateUser.password,
+      existingUser?.hashedPassword,
+    );
+
+    delete candidateUser.password;
+    if (!compairison) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        message: "Authentication Failed <oo>",
+      });
+    }
+    const csrfToken = setJwtCookie(req, res, existingUser);
+
+    return res.status(StatusCodes.OK).json({
+      name: existingUser.name,
+      email: existingUser.email,
+      csrfToken,
+      message: "logged in",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to get tokens" });
+    next(error);
+  }
 }
 
-async function createUser(candidateUser) {
+module.exports = { googleLogon };
+
+async function registerUser(req, res, candidateUser, next) {
+  //start register
   candidateUser.hashedPassword = await hashPassword(candidateUser.password);
-  delete candidateUser.password;
   //if no user account exists then we register
   let result = null;
   try {
@@ -66,88 +136,20 @@ async function createUser(candidateUser) {
 
       return { user, welcomeTasks };
     });
-    return result.user;
   } catch (err) {
     if (err.name === "PrismaClientKnownRequestError" && err.code === "P2002") {
-      // return res.status(400).json({ message: "Email already registered" });
+      return res.status(400).json({ message: "Email already registered" });
     } else {
       getPrismaErrorInfo(err);
-      // return next(err);
+      return next(err);
     }
   }
+  const csrfToken = setJwtCookie(req, res, result.user);
+
+  return res.status(StatusCodes.CREATED).json({
+    user: result.user,
+    csrfToken,
+    welcomeTasks: result.welcomeTasks,
+    transactionStatus: "success",
+  });
 }
-
-async function googleLogon(req, res, next) {
-  console.log("google-hand-shake");
-  try {
-    const code = req.body.code;
-
-    const r = await oAuth2Client.getToken({
-      code: code,
-      redirectUri: "postmessage",
-    });
-
-    oAuth2Client.setCredentials(r.tokens);
-    // 5. get user info
-    const idToken = r.tokens.id_token;
-
-    const userInfo = await oAuth2Client.verifyIdToken({
-      idToken,
-      audience: process.env.GOOGLE_CLIENT_ID,
-    });
-
-    const payload = userInfo.getPayload();
-    const splitEmail = (email) => {
-      return email?.split("@")[0];
-    };
-
-    const { name, email } = payload;
-
-    const candidateUser = {
-      name,
-      email,
-      password: `${splitEmail(payload?.email)}${process.env.GOOGLE_CLIENT_PASSWORD_SALT}`,
-    };
-
-    // 6. use info to query db
-    let user;
-    try {
-      user = getExistingUser(email) || createUser(candidateUser);
-    } catch (e) {
-      if (e.name === "PrismaClientKnownRequestError" && e.code === "P2002") {
-        return res.status(400).json({ message: "Email already registered" });
-      } else {
-        getPrismaErrorInfo(e);
-        return next(e);
-      }
-    }
-    console.log();
-    const compairison = await comparePassword(
-      candidateUser.password,
-      user?.hashedPassword,
-    );
-
-    delete candidateUser.password;
-
-    if (!compairison) {
-      return res.status(StatusCodes.UNAUTHORIZED).json({
-        message: "Authentication Failed <oo>",
-      });
-    }
-
-    const csrfToken = setJwtCookie(req, res, user);
-
-    return res.status(StatusCodes.OK).json({
-      name: user.name,
-      email: user.email,
-      csrfToken,
-      message: "logged in",
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Failed to get tokens" });
-    next(error);
-  }
-}
-
-module.exports = { googleLogon };
